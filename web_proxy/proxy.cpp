1d6ff82a5773cc807fbbe02b2c22ab489aae84b0
364gautham <364gautham@gmail.com>
2018-11-17 12:02:14 -0700

    Basic Caching

--- web_proxy/proxy.cpp
+++ web_proxy/proxy.cpp
@@ -1,31 +1,29 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <iostream>
+
+#include "headers.h"
+
 using namespace std;
 extern int h_errno;
 
-#define CONNMAX 1000
-#define BYTES 1024
 int clients[CONNMAX];
-#define MAXBUFSIZE 10000
-
 struct sockaddr_in address;
 int addrlen = sizeof(address);
 
+char* md5sum_create(const char* path){
+      unsigned char digest[16];
+      printf("string length: %lu\n", strlen(path));
+
+      MD5_CTX ctx;
+      MD5_Init(&ctx);
+      MD5_Update(&ctx, path, strlen(path));
+      MD5_Final(digest, &ctx);
+
+      char* mdString=new char[33];
+      for (int i = 0; i < 16; i++)
+          sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+      //printf("md5 digest: %s\n", mdString);
+      return mdString;
+}
 int create_socket(int port){
       int server_fd;
       // Creating socket file descriptor
@@ -85,22 +83,30 @@ void client(int fd,char* host, char* buffer,char* port,char* path,char* http_ver
       }
       // create http request
       memset((void*)buffer,MAXBUFSIZE,0);
-      sprintf(buffer,"GET https://%s %s %s\r\nConnection: close\r\n\r\n ",path,http_version,host);
-      cout<<"\n"<<buffer<<endl;
+      sprintf(buffer,"GET https://%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n ",path,http_version,host);
+      cout<<"\nProxy Request to Server:\n"<<buffer<<endl;
       if(send(sockfd,buffer,strlen(buffer),0) == -1)
           cout<<"failed to send\n";
 
-          int recv_bytes;
-          char *recv_buffer= new char[MAXBUFSIZE];
-          do{
-            memset((void*)recv_buffer,MAXBUFSIZE,0);
-            recv_bytes=recv(sockfd,recv_buffer,MAXBUFSIZE,0);
-            if(recv_bytes)
-                cout<<"\nServer Response:\n"<<recv_buffer<< "\n";
-            if(send(fd,recv_buffer,recv_bytes,0) == -1)
-                cout<<"failed to send\n";
-          }while(recv_bytes>0);
-
+      int recv_bytes;
+      char *recv_buffer= new char[MAXBUFSIZE];
+      char *filename;
+      fstream file;
+      filename=create_file(path);
+      file.open(filename,fstream::out | fstream::binary);
+      if(file.is_open()) cout<<"Proceed with storing cache\n";
+      else cout<<"Error in creating Cache: fileopen\n";
+      do{
+        memset((void*)recv_buffer,MAXBUFSIZE,0);
+        recv_bytes=recv(sockfd,recv_buffer,MAXBUFSIZE,0);
+        if(recv_bytes)
+            cout<<"\nServer Response:\n"<<recv_buffer<< "\n";
+        if(send(fd,recv_buffer,recv_bytes,0) == -1)
+            cout<<"failed to send\n";
+        //write this data to cache
+        file.write(recv_buffer,recv_bytes);
+      }while(recv_bytes>0);
+      file.close();
       cout<<"\n end \n";
       shutdown(sockfd,SHUT_RDWR);
       close(sockfd);
@@ -155,10 +161,116 @@ char* get_path(char* str){
     }
 
 }
+void handle_bad_hostname(int fd){
+    char * err = new char[500];
+   strcpy(err,"<HEAD><TITLE>HTTP 404 Not Found error message</TITLE></HEAD>\n");
+   strcat(err,"<html><BODY>HTTP 404 Not Found error message: Bad Hostname");
+   strcat(err,"\r\n");
+   strcat(err,"</BODY></html>");
+   if(send(fd,err,strlen(err),0) == -1) {
+       printf("failed to send\n");
+   }
+}
+void handle_non_GET(int fd){
+   char * err = new char[500];
+   strcpy(err,"<HEAD><TITLE>HTTP 400 Bad Request error message</TITLE></HEAD>\n");
+   strcat(err,"<html><BODY>HTTP 400 Bad Request error message: Non_GET Request");
+   strcat(err,"\r\n");
+   strcat(err,"</BODY></html>");
+   if(send(fd,err,strlen(err),0) == -1) {
+       printf("failed to send\n");
+   }
+}
+int search_f_timeout(char* hash){
+    //first check: file existence
+    if( access(hash,F_OK) != -1 ) {
+      // file exists ,  check timeout
+        return 1;
 
+    } else {
+        // file doesn't exist
+        return 0;
+    }
+}
+int get_filesize(char* filename){
+    // struct stat stat_buf;
+    // int rc = stat(filename.c_str(), &stat_buf);
+    // return rc == 0 ? stat_buf.st_size : -1;
+    return 1;
+}
+int get_filecreation_time(char* filename){
+
+}
+char* create_file(char* path){
+    fstream cache;
+    //create hash_key
+    char *hash_key=new char[200];
+    hash_key=md5sum_create(path);
+
+    cache.open(hash_key);
+    if(cache.is_open())cout<<"File creation Success\n";
+    else cout<<"Error: create_file()\n";
+    cache.close();
+
+    return hash_key;
+}
+int check_cache(char* path){
+
+    char *hash_key=new char[200];
+    hash_key=md5sum_create(path);
+    //cout << "Hash"<<hash_key;
+    if(search_f_timeout(hash_key)){
+      //to send from cache
+      delete[] hash_key;
+      return 1;
+    }
+    else{
+      //store in cache
+        delete[] hash_key;
+        return 0;
+    }
+    // struct stat attr;
+    // stat("/home/gautham/Network Systems/p_3/time", &attr);
+    // char time[100];
+    // sprintf(time,"%s",ctime(&attr.st_mtime));
+    // cout<<time<<endl;
+}
+void send_from_cache(int fd,char* path){
+    char *hash_key=new char[200];
+    hash_key=md5sum_create(path);
+    streamsize data_size;
+    //get file_size
+    fstream cache;
+    int size,recv_bytes;
+    char *recv_buffer=new char[MAXBUFSIZE];
+    if((size = get_filesize(hash_key)) == -1){
+        cout<<"Error in file : Filesize \n";
+    }
+    else{
+      //read from file
+      cache.open(hash_key,fstream::in | fstream::binary);
+      if (cache.is_open())
+      {
+          while (!cache.eof()){
+              cache.read(recv_buffer,MAXBUFSIZE);
+              data_size=cache.gcount();
+              if(send(fd,recv_buffer,data_size,0) == -1)
+                cout<<"failed to send\n";
+          }
+          cache.close();
+      }
+
+    }
+    delete[] hash_key;
+    delete[] recv_buffer;
+
+
+
+}
 void request_parse(int fd){
       int recv_bytes;
       char* buffer=new char[MAXBUFSIZE];
+      fstream link;
       //memset((void*)recv_buffer,10000,0);
       recv_bytes=recv(clients[fd],buffer,MAXBUFSIZE,0);
       if(recv_bytes)
@@ -175,15 +287,16 @@ void request_parse(int fd){
             str = strtok(NULL," ");
 
             char* hostname=get_hostname(str);
-            cout<<hostname<<endl;
+            cout<<"Hostname:"<<hostname<<endl;
             char* port=get_port(str);
-            cout<<port<<endl;
+            cout<<"Port:"<<port<<endl;
             char* path=get_path(str);
-            cout<<path<<endl;
+            cout<<"Path:"<<path<<endl;
             valid_name=gethostbyname(hostname);
-            str=strtok(NULL," ");
+            str=strtok(NULL," \r\n");
             if(!valid_name){
               cout<<"Host name failed : "<< h_errno<<endl;
+              handle_bad_hostname(clients[fd]);
               shutdown(clients[fd],SHUT_RDWR);
               close(clients[fd]);
               clients[fd]=-1;
@@ -191,16 +304,28 @@ void request_parse(int fd){
             }
             else{
               //cout<<(valid_name->h_name)<<endl;
-              client(clients[fd],hostname,buffer,port,path,str);
+                  if(check_cache(path)){
+                    //send from cache
+                    cout<<"Sending From Cache\n";
+                    send_from_cache(clients[fd],path);
+                  }
+                  else{
+                    //get from server and store in cache
+                    cout<<"FILE not found in cache, Request from server\n";
+                    client(clients[fd],hostname,buffer,port,path,str);
+                  }
+
             }
       }//end of get-check
+      else{
+        handle_non_GET(clients[fd]);
+      }
 
       shutdown(clients[fd],SHUT_RDWR);
       close(clients[fd]);
       clients[fd]=-1;
       exit(1);
 
-    //free(buffer);
 
 }
 
